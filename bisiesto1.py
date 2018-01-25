d54116f6ec6dfad1256ca558da35eb2e84489d36
ism06659681 <ism06659681@a04.informatica.escoladeltreball.org>
2018-01-25 13:50:04 +0100

    Upload

---
+++ bisiesto1.py
@@ -0,0 +1,12 @@
+#coding: utf8
+def esBisiesto (anyo):
+	anyo = input("Año")
+	if anyo%400==0 :
+		print "Bisiesto"
+	elif anyo%100 :
+		print "No bisiesto"
+	else anyo%4
+		print "Bisiesto"
+esBisiesto 
+esBisiesto
+	
